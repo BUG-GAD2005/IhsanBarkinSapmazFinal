b37bf7570a1390be0066bd122f293e20f6a2b12e
barkinspmz <barkinsapmaz2@outlook.com>
2023-06-19 18:58:37 +0300

    Menu Panel design implemented. Card script is created to create customizable card.
    Menu design looks like the example wireframe & card script has created which allows to create customizable card.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Image cardImage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
